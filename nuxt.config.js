2d6d9d1073485e1d89e791abaca511e27825bd41
Benjamin Geets <bgeets@icloud.com>
2021-03-15 15:42:49 +0100

    ajout de sass

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,15 +52,5 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     publicPath: '/public/'
-  },
-  markdownit: {
-    preset: 'default',
-    linkify: true,
-    breaks: true,
-    use: [
-      'markdown-it-div',
-      'markdown-it-attrs'
-    ],
-    runtime: true // Support `$md()`
   }
 }
